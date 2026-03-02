63d905df27e6ea6325bb56dfe642303acc8728a5
Romar Thiombiano <romaricthiombiano4@gmail.com>
2026-03-02 15:07:44 +0000

    Login web boxed : email par défaut, téléphone en option

--- resources/views/auth/boxed/user_login.blade.php
+++ resources/views/auth/boxed/user_login.blade.php
@@ -32,16 +32,10 @@
                                         <form class="form-default loginForm" id="user-login-form"role="form" action="{{ route('login') }}" method="POST">
                                             @csrf
                                             
-                                            <!-- Email or Phone -->
+                                            <!-- Email ou téléphone (email par défaut) -->
                                             @if (addon_is_activated('otp_system'))
-                                                <div class="form-group phone-form-group mb-1">
-                                                    <label for="phone" class="fs-12 fw-700 text-soft-dark">{{  translate('Phone') }}</label>
-                                                    <input type="tel" phone-number id="phone-code" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }} rounded-0" value="{{ old('phone') }}" placeholder="" name="phone" autocomplete="off">
-                                                </div>
-
-                                                <input type="hidden" name="country_code" value="">
-                                                
-                                                <div class="form-group email-form-group mb-1 d-none">
+                                                {{-- Email visible par défaut --}}
+                                                <div class="form-group email-form-group mb-1">
                                                     <label for="email" class="fs-12 fw-700 text-soft-dark">{{  translate('Email') }}</label>
                                                     <input type="email" class="form-control rounded-0 {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" placeholder="{{  translate('johndoe@example.com') }}" name="email" id="email" autocomplete="off">
                                                     @if ($errors->has('email'))
@@ -50,6 +44,14 @@
                                                         </span>
                                                     @endif
                                                 </div>
+
+                                                {{-- Téléphone masqué au départ, accessible via le lien --}}
+                                                <div class="form-group phone-form-group mb-1 d-none">
+                                                    <label for="phone" class="fs-12 fw-700 text-soft-dark">{{  translate('Phone') }}</label>
+                                                    <input type="tel" phone-number id="phone-code" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }} rounded-0" value="{{ old('phone') }}" placeholder="" name="phone" autocomplete="off">
+                                                </div>
+
+                                                <input type="hidden" name="country_code" value="">
                                                 
                                                 <div class="form-group text-right">
                                                     <button class="btn btn-link p-0 text-primary fs-12 fw-400" type="button" onclick="toggleEmailPhone(this)"><i>*{{ translate('Use Email Instead') }}</i></button>
