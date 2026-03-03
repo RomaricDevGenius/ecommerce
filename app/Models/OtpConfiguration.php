8883771dc574c6e310c13a0980e0b68dc28abd9c
Romar Thiombiano <romaricthiombiano4@gmail.com>
2026-03-03 15:04:17 +0000

    Rendre OtpConfiguration remplissable (type, value)

--- app/Models/OtpConfiguration.php
+++ app/Models/OtpConfiguration.php
@@ -9,5 +9,13 @@ class OtpConfiguration extends Model
 {
     use PreventDemoModeChanges;
 
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'type',
+        'value',
+    ];
 }
